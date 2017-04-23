acd587b0bc297f29f408a252937889cb772cd915
JokurPL <simcitymrsuperslid@gmail.com>
2017-04-23 13:40:15 +0200

    laravel

---
+++ app/Http/Middleware/CheckRole.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CheckRole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->user() === null) {
+          return redirect('/login');
+        }
+
+        $actions = $request->route()->getAction();
+        var_dump($actions);
+
+        return $next($request);
+    }
+}
---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $role = new \App\Roles();
+        $role -> name = 'Admin';
+        $role->save();
+
+        $role = new \App\Roles();
+        $role -> name = 'Moderator';
+        $role->save();
+
+        $role = new \App\Roles();
+        $role -> name = 'Użytkownik';
+        $role->save();
+    }
+}
